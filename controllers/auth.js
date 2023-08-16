3de486903f34cacea93fd59f6145e9d49b899f3c
TIRony <rony120114@gmail.com>
2023-08-16 20:34:00 +0600

    polish

--- controllers/auth.js
+++ controllers/auth.js
@@ -100,36 +100,8 @@ exports.signUp = (req, res) => {
 }
 
 exports.logout = (req, res) => {
-    res.cookie('userSave', 'logout', {
-        expires: new Date(Date.now() + 2 * 1000),
-        httpOnly: true
-    });
-    res.status(200).redirect("/");
-}
-
-exports.isLoggedIn = async (req, res, next) => {
-    if (req.cookies.userSave) {
-        try {
-            // 1. Verify the token
-            const decoded = await promisify(jwt.verify)(req.cookies.userSave,
-                process.env.JWT_SECRET
-            );
-            console.log(decoded);
-
-            // 2. Check if the user still exist
-            db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (err, results) => {
-                console.log(results);
-                if (!results) {
-                    return next();
-                }
-                req.user = results[0];
-                return next();
-            });
-        } catch (err) {
-            console.log(err)
-            return next();
-        }
-    } else {
-        next();
-    }
-}
+  // res.clearCookie("token");
+  res.status(200).json({
+    message: "Signout successfully...!",
+  });
+};
--- routes/router.js
+++ routes/router.js
@@ -5,14 +5,14 @@ const authController = require("../controllers/auth.js");
 const userMiddleware = require('../middleware/users.js');
 
 // http://localhost:3000/api/sign-up
-router.post('/sign-up', userMiddleware.validateRegister, authController.signUp)
+router.post('/signup', userMiddleware.validateRegister, authController.signUp)
 
 // http://localhost:3000/api/login
 
 router.post('/login', authController.login)
 
 // http://localhost:3000/api/logout
-router.get('/logout', authController.logout);
+router.post('/logout', authController.logout);
 
 // http://localhost:3000/api/secret-route
 router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
