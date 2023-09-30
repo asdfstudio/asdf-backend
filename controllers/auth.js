2d4300c5deb675cb22b57eec93835f57e9ed6d0b
unknown <rony120114@gmail.com>
2023-09-30 12:50:59 +0600

    polishing

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,101 +4,141 @@ const uuid = require('uuid');
 
 const db = require('../lib/db.js');
 
+const validator = require('validator');
+
 exports.login = async (req, res) => {
-    try {
-        db.query(
-            `SELECT * FROM users WHERE email = ?;`,
-            [req.body.email],
-            (err, result) => {
-              if (err) {
-                return res.status(400).send({
-                  message: 'email is incorrect!'
-                });
-              }
-              if (!result.length) {
-                return res.status(400).send({
-                  message: 'email is incorrect!'
-                });
-              }
-        
-              bcrypt.compare(
-                req.body.password,
-                result[0]['password'],
-                (bErr, bResult) => {
-                  if (bErr) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).send({
+        message: 'Please provide both email and password.',
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).send({
+        message: 'Invalid email format.',
+      });
+    }
+
+    db.query(
+      `SELECT * FROM users WHERE email = ?;`,
+      [email],
+      (err, result) => {
+        if (err) {
+          return res.status(400).send({
+            message: 'An error occurred while checking the email.',
+          });
+        }
+        if (!result.length) {
+          return res.status(400).send({
+            message: 'Email or password is incorrect.',
+          });
+        }
+
+        bcrypt.compare(
+          password,
+          result[0]['password'],
+          (bErr, bResult) => {
+            if (bErr) {
+              return res.status(400).send({
+                message: 'Email or password is incorrect.',
+              });
+            }
+            if (bResult) {
+              const token = jwt.sign(
+                {
+                  email: result[0].email,
+                  userId: result[0].id,
+                  role: result[0].role,
+                },
+                process.env.JWT_SECRET,
+                { expiresIn: '1d' }
+              );
+              db.query(`UPDATE users SET last_login = now() WHERE id = ?;`, [
+                result[0].id,
+              ]);
+              return res.status(200).send({
+                message: 'Logged in!',
+                token,
+                user: result[0],
+              });
+            }
+            return res.status(400).send({
+              message: 'Email or password is incorrect.',
+            });
+          }
+        );
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({
+      message: 'Internal server error.',
+    });
+  }
+};
+
+
+exports.signUp = (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).send({
+        message: 'Please provide your name, email, and password.',
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).send({
+        message: 'Invalid email format.',
+      });
+    }
+
+    db.query(
+      'SELECT id FROM users WHERE LOWER(email) = LOWER(?)',
+      [email],
+      (err, result) => {
+        if (result && result.length) {
+          return res.status(400).send({
+            message: 'This email is already in use!',
+          });
+        } else {
+          bcrypt.hash(password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).send({
+                message: err,
+              });
+            } else {
+              db.query(
+                'INSERT INTO users (id, name, email, password, role, registered, last_login) VALUES (?, ?, ?, ?, DEFAULT, now(), now());',
+                [uuid.v4(), name, email, hash],
+                (err, result) => {
+                  if (err) {
                     return res.status(400).send({
-                      message: 'email or password incorrect!',
+                      message: err,
                     });
                   }
-                  if (bResult) {
-                    // password match
-                    const token = jwt.sign(
-                      {
-                        email: result[0].email,
-                        userId: result[0].id,
-                        role: result[0].role
-                      },
-                      process.env.JWT_SECRET,
-                      { expiresIn: '1d' }
-                    );
-                    db.query(`UPDATE users SET last_login = now() WHERE id = ?;`, [
-                      result[0].id,
-                    ]);
-                    return res.status(200).send({
-                      message: 'Logged in!',
-                      token,
-                      user: result[0],
-                    });
-                  }
-                  return res.status(400).send({
-                    message: 'email or password incorrect!!!',
+                  return res.status(201).send({
+                    message: 'Registered!',
                   });
                 }
               );
             }
-          );
-    } catch (err) {
-        console.log(err);
-    }
-}
-exports.signUp = (req, res) => {
-    db.query(
-        'SELECT id FROM users WHERE LOWER(email) = LOWER(?)',
-        [req.body.email],
-        (err, result) => {
-          if (result && result.length) {
-            // error
-            return res.status(409).send({
-              message: 'This email is already in use!',
-            });
-          } else {
-            // email not in use
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-              if (err) {
-                return res.status(500).send({
-                  message: err,
-                });
-              } else {
-                db.query(
-                  'INSERT INTO users (id, name, email, password, role, registered, last_login) VALUES (?, ?, ?, ?, DEFAULT, now(), now());',
-                  [uuid.v4(), req.body.name, req.body.email, hash],
-                  (err, result) => {
-                    if (err) {
-                      return res.status(400).send({
-                        message: err,
-                      });
-                    }
-                    return res.status(201).send({
-                      message: 'Registered!',
-                    });
-                  }
-                );
-              }
-            });
-          }
+          });
         }
-      );
-}
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({
+      message: 'Internal server error.',
+    });
+  }
+};
+
 
 exports.updateUser = (req, res) => {
 
